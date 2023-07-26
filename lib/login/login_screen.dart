004b6451a6c0185ed44b6aeffbba9612ec43e2b2
quyetthangphan <pqt.lhu@gmail.com>
2023-07-26 23:11:37 +0700

    login and home

---
+++ lib/login/login_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginScrenn extends StatelessWidget {
+  const LoginScrenn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
